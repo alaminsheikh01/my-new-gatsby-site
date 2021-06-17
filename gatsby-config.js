cad9c8cf7e38349c8cb89078e67dc5d8805425e2
alamin <alamin@gmail.com>
2021-06-17 20:44:22 +0600

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,19 @@
 module.exports = {
   siteMetadata: {
     title: "My First Gatsby Site",
+    description:
+      "This is my first day of learning gatsby freamewrok so far so.",
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
   ],
 };
